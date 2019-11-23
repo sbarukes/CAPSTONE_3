a154f02f8dc6412c8ce66d4c9de34aae8d4ffe06
sbarukes <sbarukes@gmail.com>
2019-11-23 11:36:20 -0500

    made polymorphism

---
+++ CAPSTONE_3/Repositories/BaseStudentRepository.cs
@@ -0,0 +1,16 @@
+﻿using CAPSTONE_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CAPSTONE_3.Repositories
+{
+    public class BaseStudentRepository
+    {
+        public virtual void CreateStudent(Student student)
+        {
+
+        }
+    }
+}
---
+++ CAPSTONE_3/Repositories/StudentRepository.cs
@@ -0,0 +1,37 @@
+﻿using CAPSTONE_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CAPSTONE_3.Repositories
+{
+    public class StudentRepository : BaseStudentRepository
+    {
+        RegistrationAppContext db = new RegistrationAppContext();
+
+        //Polymorphic
+        public override void CreateStudent(Student st)
+        {
+            try
+            {
+                var check = from s in db.Students
+                            where s.Username == st.Username
+                            select s;
+                if (check.Count() > 0)
+                {
+                    db.Students.Add(st);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Username is already in use, please select another");
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception("New student was not created");
+            }
+        }
+    }
+}
