48257a56c1edbf2ec5a327841da17d4d499eb5d3
sbarukes <sbarukes@gmail.com>
2019-11-30 15:22:08 -0500

    added unit tests

---
+++ CAPSTONETests/Repositories/RegistrationTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CAPSTONE_3.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationApp.Tests
+{
+    [TestClass()]
+    public class RegistrationTests
+    {
+        [TestMethod()]
+        public void GetAllCoursesTest()
+        {
+            CourseRepository _c = new CourseRepository();
+            var test = _c.GetAll().ToList();
+            Assert.IsTrue(test.Count > 0);
+        }
+    }
+}
